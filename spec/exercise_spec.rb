f563103f7b608d92daca2fdc7b641953475e38fc
Brian Prothero <brian.prothero@gmail.com>
2011-10-17 20:31:07 -0700

    Test should be passing now

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -3,7 +3,7 @@ describe "My exercise 2 tests" do
 		context "when calling size" do
 			it "should return the number of characters that make up the string" do
 				str_len = "This string is 3 characters long.".length
-				str_len.should eq 34
+				str_len.should eq 33
 			end
 		end
 	end
@@ -12,7 +12,7 @@ describe "My exercise 2 tests" do
 		context "when performing modulus 2 of even number" do
 			it "should return zero" do
 				even_num = 5
-				(even_num % 2).should be_zero
+				(even_num % 2).should equal 1
 			end
 		end
 	end
@@ -21,7 +21,7 @@ describe "My exercise 2 tests" do
 		context "when length of symbol" do
 			it "should equal length of string with same value" do
 				:symbol
-				str_of_sym = "symbol_"
+				str_of_sym = "symbol"
 				:symbol.length.should eq str_of_sym.length
 			end
 		end
